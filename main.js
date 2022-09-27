ea74b9362631f334b1e6b1cadfb36e2a779ffe5e
Carixa <nadiamarsal18@gmail.com>
2022-09-27 16:08:15 -0300

    Codigo subite al juego

---
+++ main.js
@@ -0,0 +1,8 @@
+let age = 13;
+let height = 50; 
+
+     if (age > 10 && height > '52') { 
+    console.log ('¡Súbete, chico!'); 
+} else { 
+    console.log ('Lo siento, chico. Tal vez el proximo año.');
+}
